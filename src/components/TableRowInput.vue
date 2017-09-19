5375cca47110023d087e9af4b4b0c367692297f1
Cihad Turhan <cihadturhan@gmail.com>
2017-09-19 12:19:13 +0300

    make input more vibrant

--- src/components/TableRowInput.vue
+++ src/components/TableRowInput.vue
@@ -13,12 +13,13 @@
 <style scoped>
   td {
     padding: 0;
+    border-color: hsla(120, 40%, 50%, 0.8);
   }
 
   input {
     height: 23px;
     border: none;
-    background: hsla(120, 50%, 80%, 0.3);
+    background: hsla(120, 50%, 80%, 0.4);
     text-align: right;
     padding: 0 10px;
     appearance: none;
